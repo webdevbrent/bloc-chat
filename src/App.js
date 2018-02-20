baf5cd94b4bceea9dc5c583db011399788be916b
webdevbrent <webdeveloperbrent@gmail.com>
2018-02-19 19:33:40 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import * as firebase from 'firebase';
 import './App.css';
+
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDsoA329VlopARaLKn9oWCPLS_77CcSec8",
+  authDomain: "bloc-chat-8f25f.firebaseapp.com",
+  databaseURL: "https://bloc-chat-8f25f.firebaseio.com",
+  projectId: "bloc-chat-8f25f",
+  storageBucket: "bloc-chat-8f25f.appspot.com",
+  messagingSenderId: "1068372700222"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import RoomList  from './components/RoomList';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<RoomList />, document.getElementById('root'));
+// registerServiceWorker();

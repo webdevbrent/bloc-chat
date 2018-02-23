2cc8b0ae94c11612e3773788c2853e702be7291a
webdevbrent <webdevbrent@gmail.com>
2018-02-23 09:48:32 -0500

    Added ReactStrap to Project

--- src/App.js
+++ src/App.js
@@ -3,23 +3,22 @@ import * as firebase from 'firebase';
 import './App.css';
 import RoomList from './components/RoomList';
 
-
 // Initialize Firebase
 const config = {
-  apiKey: "AIzaSyDsoA329VlopARaLKn9oWCPLS_77CcSec8",
-  authDomain: "bloc-chat-8f25f.firebaseapp.com",
-  databaseURL: "https://bloc-chat-8f25f.firebaseio.com",
-  projectId: "bloc-chat-8f25f",
-  storageBucket: "bloc-chat-8f25f.appspot.com",
-  messagingSenderId: "1068372700222"
+  apiKey: 'AIzaSyDsoA329VlopARaLKn9oWCPLS_77CcSec8',
+  authDomain: 'bloc-chat-8f25f.firebaseapp.com',
+  databaseURL: 'https://bloc-chat-8f25f.firebaseio.com',
+  projectId: 'bloc-chat-8f25f',
+  storageBucket: 'bloc-chat-8f25f.appspot.com',
+  messagingSenderId: '1068372700222',
 };
 firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <RoomList firebase = {firebase} />
+      <div className="container-fluid">
+        <RoomList firebase ={firebase} />
       </div>
     );
   }
--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,33 +1,30 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class RoomList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            rooms: [] 
-        };
-        this.roomsRef = this.props.firebase.database().ref('rooms');
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      rooms: [],
+    };
+    this.roomsRef = this.props.firebase.database().ref('rooms');
+  }
 
-    componentDidMount() {
-        this.roomsRef.on('child_added', snapshot => {
-            const room = snapshot.val();
-            this.setState({ rooms: this.state.rooms.concat( room ) })
-          });
-    }
+  componentDidMount() {
+    this.roomsRef.on('child_added', (snapshot) => {
+      const room = snapshot.val();
+      this.setState({ rooms: this.state.rooms.concat(room) });
+    });
+  }
 
-    render() {
-        return (
-            <div>
-                {/* Get all rooms */}
-                {this.state.rooms.map((rooms) => {
-                    return rooms;
-                })}
-                <h1>{this.state.rooms}</h1>
-            </div>
-        );
-    }
-
-} // <
+  render() {
+    return (
+      <div>
+        {/* Get all rooms */}
+        {this.state.rooms.map(rooms => rooms)}
+        <h1>{this.state.rooms}</h1>
+      </div>
+    );
+  }
+} 
 
 export default RoomList;
